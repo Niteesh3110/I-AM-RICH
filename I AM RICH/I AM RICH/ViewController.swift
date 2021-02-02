1041fc710b57d39b78ff92312762643b2671142c
Niteesh3110 <61090880+Niteesh3110@users.noreply.github.com>
2021-02-03 03:03:37 +0530

    Add files via upload

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by NITS on 07/06/20.
+//  Copyright © 2020 NITS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
